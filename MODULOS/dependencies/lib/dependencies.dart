c8e4953cdc041409d5fab633c916757498c68295
pwlimaverde <pwlimaverde@gmail.com>
2024-11-09 09:39:46 -0300

    criacao do dependencies module

---
+++ MODULOS/dependencies/lib/dependencies.dart
@@ -0,0 +1,26 @@
+library dependencies;
+
+///Pacakages Externos
+export 'package:return_success_or_error/return_success_or_error.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_storage/firebase_storage.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:firebase_remote_config/firebase_remote_config.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:android_power_manager/android_power_manager.dart';
+export 'package:permission_handler/permission_handler.dart';
+export 'package:hive/hive.dart';
+
+export 'package:path_provider/path_provider.dart';
+export 'package:connectivity_plus/connectivity_plus.dart';
+export 'package:get/get.dart';
+export 'package:device_info_plus/device_info_plus.dart';
+export 'package:google_sign_in/google_sign_in.dart';
+export 'package:uuid/uuid.dart';
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+export 'package:logger/logger.dart';
+
+///Pacakages Internos
+export 'package:services/services.dart';
+export 'package:auth/auth.dart';
+export 'package:core/core.dart';
